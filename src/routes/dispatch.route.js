5b6c4c24abe0923a9d58f7130e1a572937004fa5
CDOM201 <carlos@carlosdaniel.info>
2025-02-26 11:32:15 -0700

    Add CRUD operations for dispatch: implement fetching, updating, and deleting; improve login error handling and response structure

--- src/routes/dispatch.route.js
+++ src/routes/dispatch.route.js
@@ -9,6 +9,11 @@ router.get('/', async (req, res) => {
     res.json(dispatch);
 });
 
+router.get('/:id', async (req, res) => {
+    const dispatch = await dispatchSchema.findById(req.params.id);
+    res.json(dispatch);
+});
+
 router.post('/', async (req, res) => {
     const {first_name, last_name, email, password} = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -45,22 +50,53 @@ router.post('/login', async (req, res) => {
     }
 
     const dispatch = await dispatchSchema.findOne({email});
+
     if (!dispatch) {
-        res.status(400).json({error: true, message: 'Invalid email or password', status: 400, valid: false});
+        res.status(404).json({error: true, message: 'Invalid email or password', status: 404, valid: false});
         return;
     }
+
+    if(!dispatch.active) return res.status(401).json({error: true, message: 'Dispatch not active', status: 404, valid: false});
 
     const isPasswordCorrect = await bcrypt.compare(password, dispatch.password);
     if (!isPasswordCorrect) {
-        res.status(400).json({error: true, message: 'Invalid email or password', status: 400, valid: false});
+        res.status(403).json({error: true, message: 'Invalid email or password', status: 403, valid: false});
         return;
     }
 
-    delete dispatch.password;
+    dispatch.password = null;
     
     res.json({error: false, message: 'Successfully logged in', status: 200, data: dispatch, valid: true});
 });
 
-router.delete('/', async (req, res) => {}); 
+router.delete('/:id', async (req, res) => {
+    let id = req.params.id;
+    try {
+        await dispatchSchema.findByIdAndDelete(id);
+        res.json({error: false, message: 'Successfully deleted dispatch', status: 200});
+    } catch (err) {
+        console.log({err, where: 'Deleting dispatch'});
+        res.status(500).json({error: true, message: 'Error deleting dispatch', status: 500});
+        return;
+    }
+});
+
+router.patch('/:id', async (req, res) => {
+    let body = req.body;
+    let id = req.params.id;
+
+    if(body.password) {
+        body.password = await bcrypt.hash(body.password, 10);
+    }
+
+    try {
+        await dispatchSchema.findByIdAndUpdate(id, body, {new: true});
+        res.json({error: false, message: 'Successfully updated dispatch', status: 200});
+    } catch (err) {
+        console.log({err, where: 'Updating dispatch'});
+        res.status(500).json({error: true, message: 'Error updating dispatch', status: 500});
+        return;
+    }
+});
 
 module.exports = router;
